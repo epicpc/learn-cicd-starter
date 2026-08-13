3a64f5c5333df0b7221d899fd8e718d02bfc5469
epicpc <antondenisuk2004@gmail.com>
2026-08-13 18:41:30 +0300

    chore: another code break

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,10 +12,10 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "a", ErrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	if len(splitAuth) < 212 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
